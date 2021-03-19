88b2ece17e6733509ce752a77da5a4c265409588
xumedT <sumedtamang@xumeds.lan>
2021-03-19 14:46:44 -0500

    adding the election audit code.

--- PyPoll.py
+++ PyPoll.py
@@ -33,16 +33,66 @@ file_to_save = os.path.join("analysis", "election_analysis.txt")
 #     txt_file.write("Counties in the Election\n")
 #     txt_file.write("---------------------------\n")
 #     txt_file.write("Arapahoe\nDenver\nJefferson")
+
+# 1. Initialize a total vore counter
+total_votes = 0
+# Var to store candidate names
+candidate_options = []
+# Dict for candidate votes
+candidate_votes = {}
+# Winning candidate and winning candidate Count tracker
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
+
+
 # Open Election results and read the file.
 with open(file_to_load) as election_data:
     # To do: read and analyze data here
     file_reader = csv.reader(election_data)
+    # Read the header row
     headers = next(file_reader)
-    print(headers)
     #Print each row in the CSV file.
-    # for row in file_reader:
-    #     print(row)
+    for row in file_reader:
+        # Add to the total vote count.
+        total_votes += 1 
+        # Print the candidate name from each row.
+        candidate_name = row[2]
+        # if candidate name does not match any existing candidate...
+        if candidate_name not in candidate_options:
+            # Add the candidate name to the candidate list. 
+            candidate_options.append(candidate_name)
+            # Beign tracking votes
+            candidate_votes[candidate_name] = 0
+        # Add vote to that candidates count.
+        candidate_votes[candidate_name] += 1 
 
+print(candidate_votes)
+print("")
+
+for candidate_name in candidate_votes:
+    # Retrieve vote count of a candidate
+    votes = candidate_votes[candidate_name]  
+    # Calculate the % 
+    vote_percentage = float( votes ) / float(total_votes) * 100 
+    # print output
+    print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+
+    # Determine winning vote count and candidate
+    # Determing if the votes is greater than the winning count
+    if (votes > winning_count) and (vote_percentage > winning_percentage):
+        winning_count = votes
+        winning_percentage = vote_percentage
+        winning_candidate = candidate_name
+
+winning_candidate_summary = (
+    f"------------------------------\n"
+    f"Winner: {winning_candidate}\n"
+    f"Winnig Vote Count: {winning_count:,}\n"
+    f"Winning Percentage: {winning_percentage:.1f}%\n"
+    f"------------------------------\n"
+)
+print(winning_candidate_summary)
 
 
 
